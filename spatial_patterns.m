d0c36ad16a21d894dafe35d92550a61d69b6c07d
Andri Gunnarsson <andri140183@gmail.com>
2018-11-26 16:37:58 +0000

    t

--- spatial_patterns.m
+++ spatial_patterns.m
@@ -1,7 +1,7 @@
 clear all, close all, clc
 cd('/Users/andrigun/Dropbox/glac_mass/grd')
 cd('bw')
-
+name = 'B_w'
 
 %%
 b=dir('*.asc')
@@ -37,19 +37,67 @@ for i = 5:length(B)
     ki = ki+1;
      [Z,R] = arcgridread(B(i).name); 
      S(:,:,ki) = Z;
+     Year(ki) = B(i).year
 
 end
 %% Mean
-    figure
+%close all
+name = 'B_w'
+% Meðaltal allra ára
+    f = figure('Position', [50, 100, 2200, 1200]), hold on;
+    subplot(2,3,1)
     mapshow(mean(S,3),R,'DisplayType','surface')
-    xlabel('x (easting in meters)')
-    ylabel('y (northing in meters)')
-    %demcmap(jet)
     cmap =cbrewer('seq','Blues', 10);
     colorbar
     colormap(cmap)
     shading flat
+    ax = gca
+    ax.Visible = 'off'
+% Staðalfrávik allra ára
+    subplot(2,3,2)
+    mapshow(std(S,[],3),R,'DisplayType','surface')
+    cmap =cbrewer('seq','Blues', 10);
+    colorbar
+    colormap(cmap)
+    shading flat
+    ax = gca
+    ax.Visible = 'off'
+% Staðalfrávik allra ára
+    subplot(2,3,3)
+    ltm = nanmean(nanmean(nanmean(S,3)))
+    mapshow(mean(S,3)-ltm,R,'DisplayType','surface')
+    cmap =cbrewer('seq','Blues', 10);
+    colorbar
+    colormap(cmap)
+    shading flat
+    ax = gca
+    ax.Visible = 'off'
+    
+% Viðmiðunarár
+    subplot(2,3,4)
+    ref_year = 2018
+    iy = find(Year == ref_year)
+    mapshow(S(:,:,iy),R,'DisplayType','surface')
+    cmap =cbrewer('seq','Blues', 10);
+    colorbar
+    colormap(cmap)
+    shading flat
+    ax = gca
+    ax.Visible = 'off'
 
+% Viðmiðunarár
+    subplot(2,3,5)
+    mapshow(S(:,:,iy)-mean(S,3),R,'DisplayType','surface')
+    cmap =cbrewer('div','RdBu', 10);
+    colorbar
+    colormap(gca,cmap)
+    shading flat
+    ax = gca
+    ax.Visible = 'off'
+    %zlim([-2 2])
+
+    
+    
 %% Trend
 ss = size(S)
 t = ss(3)
